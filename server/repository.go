195452d74995e00a6ad066332a6b774f8079ac7f
Argana Phangquestian <arganaphang@gmail.com>
2021-03-12 22:26:24 +0700

    Add Product Items

--- server/repository.go
+++ server/repository.go
@@ -7,6 +7,71 @@ import (
 	"github.com/segmentio/ksuid"
 )
 
+var (
+	products = []Product{
+		{
+			ID:       ksuid.New().String(),
+			Name:     "Asus ROG",
+			ImageURL: "https://cdn.medcom.id/dynamic/content/2019/07/03/1038320/fO7HtXghpT.jpg?w=480",
+			Price:    20_000_000,
+		},
+		{
+			ID:       ksuid.New().String(),
+			Name:     "Macbook Pro m1",
+			ImageURL: "https://www.apple.com/v/macbook-pro-13/g/images/overview/hero_endframe__bsza6x4fldiq_large_2x.jpg",
+			Price:    40_000_000,
+		},
+		{
+			ID:       ksuid.New().String(),
+			Name:     "Alienware",
+			ImageURL: "https://images-na.ssl-images-amazon.com/images/I/71hhY4ikVwL._AC_SL1500_.jpg",
+			Price:    50_000_000,
+		},
+		{
+			ID:       ksuid.New().String(),
+			Name:     "Asus Vivobook",
+			ImageURL: "https://images-na.ssl-images-amazon.com/images/I/81oVSSITRQL._AC_SL1500_.jpg",
+			Price:    6_000_000,
+		},
+		{
+			ID:       ksuid.New().String(),
+			Name:     "Lenovo Legion",
+			ImageURL: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6398/6398969_sd.jpg",
+			Price:    17_000_000,
+		},
+		{
+			ID:       ksuid.New().String(),
+			Name:     "Xiaomi Mi 10 Ultra",
+			ImageURL: "https://www.gizmochina.com/wp-content/uploads/2020/08/a-500x500.jpg",
+			Price:    9_000_000,
+		},
+		{
+			ID:       ksuid.New().String(),
+			Name:     "Samsung A51",
+			ImageURL: "https://images.samsung.com/is/image/samsung/id-galaxy-a51-a515-sm-a515fzbwxid-209736060?$720_576_PNG$",
+			Price:    5_000_000,
+		},
+		{
+			ID:       ksuid.New().String(),
+			Name:     "Iphone 12",
+			ImageURL: "https://cdn.tmobile.com/content/dam/t-mobile/en-p/cell-phones/apple/Apple-iPhone-12/Blue/Apple-iPhone-12-Blue-backimage.png",
+			Price:    17_000_000,
+		},
+		{
+			ID:       ksuid.New().String(),
+			Name:     "Stealseries Arctis Pro DTS",
+			ImageURL: "https://lh3.googleusercontent.com/proxy/FTdEwgA1FBIRBEI2b3i6_G7lHE9d_RCHbDJCAUZCvbm07P2LR6hgASdnXketnuBkKRZi1Gb-vIpslf6y3f2VW5T43khylBzUOj2dhYHQK3A7mhTnR-LCmttB6xDyE_VwMXMm1I2HOkQLS-9FOUzpRV6oqP0PiWMTcrgJIilD0Ts2sSzop1UnkF6BPO41RLmWCe6OSwsQ19skQCdzVfMb9sJbRzohdfpnBCH9JU-WAWs8hkPrItQ",
+			Price:    2_710_000,
+		},
+		{
+			ID:       ksuid.New().String(),
+			Name:     "Razer Blackwidow - Green Switch",
+			ImageURL: "https://assets2.razerzone.com/images/blackwidow-2019/BlackWidow2019_OGimage-1200x630.jpg",
+			Price:    1_730_000,
+		},
+	}
+)
+
 // Repository interface
 type Repository interface {
 	Login(login *Login) (*User, error)
@@ -87,7 +152,7 @@ func (r *repo) Register(register *Register) (*User, error) {
 		Username: register.Username,
 		Fullname: register.Fullname,
 	}
-	// Set New List into key `users`
+	// Set New user with key is UserID
 	err = r.badgerDB.Update(func(txn *badger.Txn) error {
 		err := txn.Set([]byte(user.ID), []byte(user.Encode()))
 		return err
@@ -120,10 +185,15 @@ func (r *repo) GetUsers() (*[]User, error) {
 	return &users, nil
 }
 func (r *repo) GetProducts() (*[]Product, error) {
-	return nil, nil
+	return &products, nil
 }
 func (r *repo) GetProductByID(id string) (*Product, error) {
-	return nil, nil
+	for _, v := range products {
+		if id == v.ID {
+			return &v, nil
+		}
+	}
+	return nil, errors.New("Product not found")
 }
 func (r *repo) GetCart(userID string) (*Cart, error) {
 	return nil, nil
