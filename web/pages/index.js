f52018e383d095cec51adbdea08aa82c1ca80ccd
Argana Phangquestian <arganaphang@gmail.com>
2021-03-13 14:41:45 +0700

    styling navbar

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,65 +1,30 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    <>
+      <nav>
+        <Link href="keranjang">
+          <a>Keranjang</a>
+        </Link>
+      </nav>
+      <style jsx>
+        {`
+          nav {
+            padding: 24px 32px;
+          }
+          nav a {
+            background-color: rgba(200, 200, 200, 0.2);
+            padding: 16px 32px;
+            border-radius: 6px;
+            transition: all 0.25s ease;
+          }
+          nav a:hover {
+            background-color: rgba(0, 0, 0, 0.8);
+            color: #fff;
+          }
+        `}
+      </style>
+    </>
+  );
 }
---
+++ web/pages/keranjang.js
@@ -0,0 +1,3 @@
+export default function Keranjang() {
+  return <p>keranjang page</p>;
+}
